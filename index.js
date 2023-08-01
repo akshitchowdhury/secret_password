501590d1ab3ede828bd82f4fa7ac212da1cec2f8
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-08-01 14:33:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
 import express from 'express'
+import bodyParser from 'body-parser'
 import {dirname} from 'path'
 import {fileURLToPath} from 'url'
-import bodyParser from 'body-parser'
+
 const app = express()
-
-const port = 3001
+const port = 3000
 const _dirname = dirname(fileURLToPath(import.meta.url))
-var bandname
-app.get("/", (req, res)=>{
-
-    res.sendFile(_dirname+"/index.html")
+var pass = ""
+app.get("/", (req,res)=>{
+    res.sendFile(_dirname+"/public/index.html")
 })
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-
-
-function test (req, res, next){
-
-    console.log(req.body)
-    bandname = req.body["email"] + req.body["password"]
-    next()
+function custom(req,res,next){
+   pass =  req.body["password"]
+   next()
 }
 
+app.use(custom)
 
-app.use(test)
+app.post("/check", (req,res)=>{
 
-app.post("/submit", (req,res)=>{
-    res.send(`<h1>Your band name generator is </h1> <h2> ${bandname} </h2>`)
+    if(pass=="ILoveProgramming")
+{
+
+    res.sendFile(_dirname+"/public/secret.html")
+}   
+else{
+    res.sendFile(_dirname+"/public/index.html")
+}
 })
-app.listen(port,()=>{
-    console.log(`listenin on port ${port}`)
+
+app.listen(port, ()=>{
+
+    console.log(`listening to port ${3000}`)
 })
 
