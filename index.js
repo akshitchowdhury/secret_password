21a4f0d07f6a52334eafa3e5bb82475f4202736b
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-08-01 14:19:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,25 @@
 
 import express from 'express'
 import bodyParser from 'body-parser'
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
 
 const app = express()
 const port = 3000
+const _dirname = dirname(fileURLToPath(import.meta.url))
+
+app.get("/", (req,res)=>{
+    res.sendFile(_dirname+"/public/index.html")
+})
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.post("/check", (req,res)=>{
+    
+})
+
+app.listen(port, ()=>{
+
+    console.log(`listening to port ${3000}`)
+})
+
