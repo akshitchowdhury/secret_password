baeb8b798ba2c93b636a7eaa5fff22b3c4076f6a
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-08-01 14:01:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+const port = 3000
